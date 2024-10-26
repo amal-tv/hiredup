4319bdda854000684e9942118eaee76c5155e3a4
Amal TV <amaltv025@gmail.com>
2024-10-26 21:46:46 +0530

    before the supabase and clerk connection

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import { Button } from "./components/ui/button"
 import AppLayout from "./layouts/AppLayout"
 import { Landing } from "./pages/Landing"
 import { Onboarding } from "./pages/Onboarding"
+import { Job } from "./pages/Job"
+import { JobListing } from "./pages/JobListing"
+import { Postjob } from "./pages/Postjob"
+import Savedjobs from "./pages/Savedjobs"
+import { Myjobs } from "./pages/Myjobs"
 
 
 
@@ -17,6 +22,26 @@ const router = createBrowserRouter([
       {
         path : '/onboarding',
         element : <Onboarding />,
+      },
+      {
+        path : '/jobs',
+        element : <JobListing />,
+      },
+      {
+        path : '/job/:id',
+        element : <Job />,
+      },
+      {
+        path : '/post-job',
+        element : <Postjob />,
+      },
+      {
+        path : '/my-jobs',
+        element : <Myjobs />,
+      },
+      {
+        path : '/saved-jobs',
+        element : <Savedjobs />,
       }
     ]
   }
--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-
+import '../App.css'
+import { Header } from '@/components/Header'
 export default function AppLayout() {
   return (
-    <div>AppLayout
+    <div>
+        <div className="grid-background"></div>
+        <main className='min-h-screen container'>
+            <Header/>
         <Outlet/>
+
+        </main>
+        <div className='p-10 text-center bg-gray-800 mt-10'>amal-dev</div>
     </div>
   )
 }
